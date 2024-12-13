2d15060bd54b62c71f43c23520c04370049fa7b3
Liia <1991safronova@gmail.com>
2024-12-13 23:54:56 +0100

    chanche 1-3

--- js/task -3.js
+++ js/task -3.js
@@ -1,18 +1,10 @@
-// Функція filterArray приймає два параметри: масив чисел numbers і значення value
 function filterArray(numbers, value) {
-  // Створюємо порожній масив для збереження чисел, які більші за value
   const filteredNumbers = [];
-
-  // Проходимося по кожному числу у масиві numbers за допомогою циклу for
   for (const number of numbers) {
-    // Перевіряємо, чи число більше за value
     if (number > value) {
-      // Якщо умова виконується, додаємо число до масиву filteredNumbers
       filteredNumbers.push(number);
     }
   }
-
-  // Повертаємо новий масив із числами, що відповідають умові
   return filteredNumbers;
 }
 
